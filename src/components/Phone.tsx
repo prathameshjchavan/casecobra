12338a4674c28a66e33be434cc5ae302d1fa85ad
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-06-18 10:42:04 +0530

    phone component

---
+++ src/components/Phone.tsx
@@ -0,0 +1,44 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React, { HTMLAttributes } from "react";
+
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+  dark?: boolean;
+}
+
+const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
+  return (
+    <div
+      className={cn(
+        "pointer-events-none relative z-50 overflow-hidden",
+        className,
+      )}
+      {...props}
+    >
+      <Image
+        src={
+          dark
+            ? "/phone-template-dark-edges.png"
+            : "/phone-template-white-edges.png"
+        }
+        width={896}
+        height={1831}
+        alt="phone image"
+        className="pointer-events-none z-50 select-none"
+      />
+
+      <div className="absolute inset-0 -z-10">
+        <Image
+          className="object-cover"
+          width={896}
+          height={1831}
+          src={imgSrc}
+          alt="overlaying phone image"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Phone;
