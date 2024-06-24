6790b2d26185bea3ad4f4996ea4d37f62392d7a1
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-06-24 12:35:15 +0530

    add options

--- src/components/DesignConfigurator.tsx
+++ src/components/DesignConfigurator.tsx
@@ -5,6 +5,11 @@ import { AspectRatio } from "./ui/aspect-ratio";
 import { cn } from "@/lib/utils";
 import { Rnd } from "react-rnd";
 import HandleComponent from "./HandleComponent";
+import { ScrollArea } from "./ui/scroll-area";
+import { Radio, RadioGroup } from "@headlessui/react";
+import { useState } from "react";
+import { COLORS } from "@/validators/option-validator";
+import { Label } from "./ui/label";
 
 interface DesignConfiguratorProps {
   configId: string;
@@ -17,6 +22,10 @@ const DesignConfigurator = ({
   imageUrl,
   imageDimentions,
 }: DesignConfiguratorProps) => {
+  const [options, setOptions] = useState<{ color: (typeof COLORS)[number] }>({
+    color: COLORS[0],
+  });
+
   return (
     <div className="relative mb-20 mt-20 grid grid-cols-3 pb-20">
       <div className="relative col-span-2 flex h-[37.5rem] w-full max-w-4xl items-center justify-center overflow-hidden rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
@@ -65,6 +74,55 @@ const DesignConfigurator = ({
             />
           </div>
         </Rnd>
+      </div>
+
+      <div className="flex h-[37.5rem] flex-col bg-white">
+        <ScrollArea className="relative flex-1 overflow-auto">
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 bottom-0 z-10 h-12 bg-gradient-to-t from-white"
+          />
+
+          <div className="px-8 pb-12 pt-8">
+            <h2 className="text-3xl font-bold tracking-tight">
+              Customize your case
+            </h2>
+
+            <div className="my-6 h-px w-full bg-zinc-200" />
+
+            <div className="relative mt-4 flex h-full flex-col justify-between">
+              <RadioGroup
+                value={options.color}
+                onChange={(val) => {
+                  setOptions((prev) => ({ ...prev, color: val }));
+                }}
+              >
+                <Label>Color: {options.color.label}</Label>
+                <div className="mt-3 flex items-center space-x-3">
+                  {COLORS.map((color) => (
+                    <Radio value={color} key={color.label}>
+                      {({ checked }) => (
+                        <span
+                          className={cn(
+                            "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full border-2 border-transparent p-0.5 focus:outline-none focus:ring-0 active:outline-none active:ring-0",
+                            { [`border-${color.tw}`]: checked },
+                          )}
+                        >
+                          <span
+                            className={cn(
+                              `bg-${color.tw}`,
+                              "h-8 w-8 rounded-full border border-black border-opacity-10",
+                            )}
+                          />
+                        </span>
+                      )}
+                    </Radio>
+                  ))}
+                </div>
+              </RadioGroup>
+            </div>
+          </div>
+        </ScrollArea>
       </div>
     </div>
   );
