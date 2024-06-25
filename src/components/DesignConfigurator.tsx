32462d51ecbf47ad280f19febd8c97f1762cbf5f
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-06-25 12:16:32 +0530

    add responsiveness to design configurator

--- src/components/DesignConfigurator.tsx
+++ src/components/DesignConfigurator.tsx
@@ -49,7 +49,7 @@ const DesignConfigurator = ({
   });
 
   return (
-    <div className="relative mb-20 mt-20 grid grid-cols-3 pb-20">
+    <div className="relative mb-20 mt-20 grid grid-cols-1 lg:grid-cols-3 pb-20">
       <div className="relative col-span-2 flex h-[37.5rem] w-full max-w-4xl items-center justify-center overflow-hidden rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
         <div className="pointer-events-none relative aspect-[896/1831] w-60 bg-opacity-50">
           <AspectRatio
@@ -98,7 +98,7 @@ const DesignConfigurator = ({
         </Rnd>
       </div>
 
-      <div className="flex h-[37.5rem] flex-col bg-white">
+      <div className="flex h-[37.5rem] w-full col-span-full lg:col-span-1 flex-col bg-white">
         <ScrollArea className="relative flex-1 overflow-auto">
           <div
             aria-hidden="true"
