2f8bcd5a80045ef0318bc84e3d1117a0eda12cea
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-07-07 19:24:23 +0530

    create weekly goal progress card

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,14 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+import { db } from "@/db";
+import { formatPrice } from "@/lib/utils";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound } from "next/navigation";
 
@@ -9,7 +20,62 @@ const DashboardPage = async () => {
 
   if (!user || user.email !== ADMIN_EMAIL) return notFound();
 
-  return <div className="flex min-h-screen w-full bg-muted/40">Dashboard</div>;
+  const orders = await db.order.findMany({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      user: true,
+      shippingAddress: true,
+    },
+  });
+
+  const lastWeekSum = await db.order.aggregate({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+      },
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+
+  const WEEKLY_GOAL = 500;
+
+  return (
+    <div className="flex min-h-screen w-full bg-muted/40">
+      <div className="mx-auto flex w-full max-w-7xl flex-col sm:gap-4 sm:py-4">
+        <div className="flex flex-col gap-16">
+          <div className="grid gap-4 sm:grid-cols-2">
+            <Card>
+              <CardHeader className="pb-2">
+                <CardDescription>Last week</CardDescription>
+                <CardTitle className="text-4xl">
+                  {formatPrice(lastWeekSum._sum.amount ?? 0)}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-sm text-muted-foreground">
+                  of {formatPrice(WEEKLY_GOAL)} goal
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Progress value={(lastWeekSum._sum.amount ?? 0) / WEEKLY_GOAL * 100} />
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
