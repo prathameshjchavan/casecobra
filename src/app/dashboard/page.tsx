1f38b9757915edf9e33c2ae104c2a0b0462bfecd
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-07-07 18:37:29 +0530

    make dashboard route protected

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,14 @@
-const DashboardPage = () => {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound } from "next/navigation";
+
+const DashboardPage = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const ADMIN_EMAIL = process.env.ADMIN_EMAIL!;
+
+  if (!user || user.email !== ADMIN_EMAIL) return notFound();
+
   return <div className="flex min-h-screen w-full bg-muted/40">Dashboard</div>;
 };
 
